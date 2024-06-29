2df4be799b32f3ea5f7fab86e62f63fc1359fc7f
mainoahmuna <mainoah913@gmail.com>
2024-06-29 00:43:03 -0700

    Initial Commit

---
+++ todood/todoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoodApp.swift
+//  todood
+//
+//  Created by Mainoah Muna on 6/29/24.
+//
+
+import SwiftUI
+
+@main
+struct todoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
