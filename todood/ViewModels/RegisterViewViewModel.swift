50dda2ff22b48e7b05a60020b93ac92c5d268ae3
mainoahmuna <mainoah913@gmail.com>
2024-06-29 14:41:22 -0700

    add profile view and todo list view

--- todood/ViewModels/RegisterViewViewModel.swift
+++ todood/ViewModels/RegisterViewViewModel.swift
@@ -5,9 +5,12 @@
 //  Created by Mainoah Muna on 6/29/24.
 //
 
+import FirebaseFirestore
+import FirebaseAuth
 import Foundation
 
 class RegisterViewViewModel: ObservableObject {
+    @Published var name = ""
     @Published var email = ""
     @Published var password = ""
     
@@ -15,11 +18,48 @@ class RegisterViewViewModel: ObservableObject {
         
     }
     
-    func login() {
+    func register() {
+        guard validate() else {
+            return
+        }
         
+        Auth.auth().createUser(withEmail: email, password: password) { [weak self] result, error in
+            guard let userId = result?.user.uid else {
+                return
+            }
+            
+            self?.insertUserRecord(id: userId)
+        }
     }
     
-    func validate () {
+    private func insertUserRecord(id: String) {
+        let newUser = User(id: id,
+                           name: name,
+                           email: email,
+                           joined: Date().timeIntervalSince1970)
         
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document(id)
+            .setData(newUser.asDictionary())
+    }
+    
+    private func validate () -> Bool {
+        guard !name.trimmingCharacters(in: .whitespaces).isEmpty,
+              !email.trimmingCharacters(in: .whitespaces).isEmpty,
+              !password.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return false
+        }
+        
+        guard email.contains("@") && email.contains(".") else {
+            return false
+        }
+        
+        guard password.count >= 8 else {
+            return false
+        }
+        
+        return true
     }
 }
